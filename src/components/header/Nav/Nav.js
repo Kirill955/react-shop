0bcf55e3040ea72a89e2dd01f0985bce3fce7fc8
Кирилл Омаров <kirill.kirill.omarov@mail.ru>
2020-04-28 19:17:28 +0600

    Add burger menu for Nav

--- src/components/header/Nav/Nav.js
+++ src/components/header/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.sass'
 import {
   BrowserRouter as Router,
@@ -8,24 +8,23 @@ import {
 } from "react-router-dom";
 
 const Nav = () => {
+    const [isOpen, toggleMenu] = useState(false)
     return (
-        <nav>
+        <nav className={isOpen ? "nav-active" : null}> 
             <ul>
-                <li><NavLink to="/" exact>Home</NavLink></li>
-                <li><NavLink to="/sale" exact>Sale</NavLink></li>
-                <li><NavLink to="/handbags" exact>Handbags</NavLink></li>
-                <li><NavLink to="/wallets" exact>Wallets</NavLink></li>
-                <li><NavLink to="/accessories" exact>Accessories</NavLink></li>
-                <li><NavLink to="/mens-store" exact>Mens Store</NavLink></li>
-                <li><NavLink to="/shoes" exact>Shoes</NavLink></li>
-                <li><NavLink to="/vintage" exact>Vintage</NavLink></li>
-                <li><NavLink to="/services" exact>Services</NavLink></li>
-                <li><NavLink to="/contact">Contact Us</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/" exact>Home</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/sale" exact>Sale</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/handbags" exact>Handbags</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/wallets" exact>Wallets</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/accessories" exact>Accessories</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/mens-store" exact>Mens Store</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/shoes" exact>Shoes</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/vintage" exact>Vintage</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/services" exact>Services</NavLink></li>
+                <li onClick={() => toggleMenu(false)}><NavLink to="/contact">Contact Us</NavLink></li>
             </ul>
-            <div className="burger-menu">
-                <span></span>
-                <span></span>
-            </div> 
+            <div className={`burger-menu ${isOpen ? "active" : null}`} onClick={() => toggleMenu(!isOpen)}>
+            </div>
         </nav>
     )
 }
